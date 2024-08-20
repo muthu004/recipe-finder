c12ac160fbb97b2f381c9f61c49ebc978bd99fcc
Muthukumar V <www.goe2004@gmail.com>
2024-08-21 00:49:19 +0530

    styles

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,27 @@
-const Card=({reciper:{recipe:{image,label}}})=>{
+const Card = ({
+  reciper: {
+    recipe: { image, label, cuisineType, mealType, dishType },
+  },
+}) => {
+  return (
+    <div className="reciper border rounded-xl bg-slate-300 shadow-md p-5 w-64 h-80 mt-10 ">
+      <h3 className=" capitalize  pb-4 font-bold ">{label}</h3>
 
-    return (
-        <div className="reciper" >
-            <h3>{label}</h3>
+      <div className=" flex flex-row gap-5 justify-center">
+        <img className="w-32 rounded-xl" src={image} alt="" />
+        <p className="bg-white p-2 text-xs rounded-xl h-8 mt-10 inline-block ">
+          {mealType}
+        </p>
+      </div>
 
-<img src={image} alt="" />
+      <p className=" font-serif  before:content-['CuisineType:'] before:pr-2 capitalize pt-4 ">
+        {cuisineType}
+      </p>
 
-        </div>
-    )
-}
-export default Card
+      <p className=" font-serif before:content-['MealType:'] before:pr-2 capitalize pt-4">
+        {dishType}
+      </p>
+    </div>
+  );
+};
+export default Card;
--- src/food.jsx
+++ src/food.jsx
@@ -1,43 +1,47 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useEffect } from "react";
 import Card from "./Card";
-function Food(){
-const[searchrecipes,setsearchRecipes]=useState('')
-const [recipes,setRecipes]=useState([]);
-async function findRecipes(){
-    const recipes= await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchrecipes}&app_id=7c6d3dc6&app_key=69979be74ae5fcd1b58de356b90ccf82%09`)
-    const response=await recipes.json();
+function Food() {
+  const [searchrecipes, setsearchRecipes] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  async function findRecipes() {
+    const recipes = await fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${searchrecipes}&app_id=7c6d3dc6&app_key=69979be74ae5fcd1b58de356b90ccf82%09`
+    );
+    const response = await recipes.json();
     console.log(response);
-    setRecipes(response.hits)
-    
-}
+    setRecipes(response.hits);
+  }
 
-useEffect(() => {
+  useEffect(() => {
     findRecipes();
-  },[]);    
-  
-  return(
-<div >
+  }, []);
 
-    <div className="flex flex-row justify-center items-center m-20 gap-9">
-    <input type="text" className="border border-black " value={searchrecipes} onChange={e=>setsearchRecipes(e.target.value)}/>
-    <button className="border border-gray-900 h-6 w-14" onClick={findRecipes}>Search</button>
+  return (
+    <div>
+      <div className="flex flex-row justify-center items-center gap-10 mt-20  ">
+        <input
+          type="text"
+          placeholder="Enter Ingredient"
+          className="border border-black font-mono pl-5 h-10 rounded-md "
+          value={searchrecipes}
+          onChange={(e) => setsearchRecipes(e.target.value)}
+        />
+        <button
+          className="border border-gray-900 h-8 w-20 rounded-md text-sm"
+          onClick={findRecipes}
+        >
+          Search
+        </button>
+      </div>
+
+      <div className="container flex flex-row gap-4  flex-wrap justify-center">
+        {recipes.map((reciper, index) => (
+          <Card key={index} reciper={reciper} />
+        ))}
+      </div>
     </div>
-
-
-
-        <div className="container">
-          {recipes.map((reciper,index) => (
-            
-            <Card key={index} reciper={reciper} />
-          ))}
-        </div>
-      
-
-
-
-</div>
-    )
+  );
 }
 
-export default Food
+export default Food;
